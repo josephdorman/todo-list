45de8c8641680302ad651bba7f4054ea03a9d59d
Joseph Dorman <JDorman555@yahoo.com>
2023-06-03 21:39:58 -0500

    Add default projects

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,26 @@
 const projects = (() => {
-  const projectsList = [];
+  const projectsList = [
+    {
+      name: 'Homework',
+      tasks: [
+        {
+          projectName: 'Homework',
+          name: 'Deliver tea to Mrs Daniels',
+          date: '06-21-2023'
+        }
+      ]
+    },
+    {
+      name: 'Chores',
+      tasks: [
+        {
+          projectName: 'Chores',
+          name: 'Clean up the washroom',
+          date: '02-15-2023'
+        }
+      ]
+    }
+  ];
 
   // implete default projects or local storage stuff here
 
@@ -14,9 +35,6 @@ const projects = (() => {
     const project = new Project(name);
     projectsList.push(project);
   }
-
-  const testProject = 'Test Project';
-  addProject(testProject);
 
   function checkIfProjectExists (name) {
     for (let i = 0; i < projectsList.length; i++) {
